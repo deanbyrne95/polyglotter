f931470e0acad83f397c6e17e821cdf52dacb35a
LexanderoW <alexanderowens1993@gmail.com>
2023-07-05 11:03:35 +0100

    comment out old code, added sidebar and materialUI

--- polyglotter-ui/src/App.jsx
+++ polyglotter-ui/src/App.jsx
@@ -1,38 +1,43 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import PageNotFound from "./pages/PageNotFound";
-import Footer from "./components/Footer";
+import './App.css'
+import Sidebar from './components/Sidebar';
+// import Register from "./pages/Register";
+// import Login from "./pages/Login";
+// import PageNotFound from "./pages/PageNotFound";
+// import Footer from "./components/Footer";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Login></Login>,
-    },
-    {
-        path: "/register",
-        element: <Register></Register>,
-    },
-    {
-        path: "/login",
-        element: <Login></Login>,
-    },
-    {
-        path: "*",
-        element: <PageNotFound></PageNotFound>,
-    },
-]);
+// const router = createBrowserRouter([
+//     {
+//         path: "/",
+//         element: <Login></Login>,
+//     },
+//     {
+//         path: "/register",
+//         element: <Register></Register>,
+//     },
+//     {
+//         path: "/login",
+//         element: <Login></Login>,
+//     },
+//     {
+//         path: "*",
+//         element: <PageNotFound></PageNotFound>,
+//     },
+// ]);
 
 const App = () => {
     return (
-        <main>
-            <div className="relative w-full h-[100%] mx-auto bg-gradient-to-br from-tertiary to-secondary">
+        <main className="app">
+            {/* <div className="relative w-full h-[100%] mx-auto bg-gradient-to-br from-tertiary to-secondary">
                 <RouterProvider router={router}></RouterProvider>
             </div>
             <div className="bg-primary bottom-[0px] fixed min-w-full">
                 <Footer />
+            </div> */}
+            <div className="app__body">
+            <Sidebar />
             </div>
         </main>
     );
