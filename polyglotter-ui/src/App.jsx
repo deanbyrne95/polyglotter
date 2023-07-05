6a6006fd02ac7f306895a3586ae7beacccc46a76
LexanderoW <alexanderowens1993@gmail.com>
2023-07-05 17:17:13 +0100

    Chat-UI is setup

--- polyglotter-ui/src/App.jsx
+++ polyglotter-ui/src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import './App.css'
-import Sidebar from './components/Sidebar';
+import "./App.css";
+
+import Sidebar from "./components/Sidebar";
+import Chat from "./components/Chat";
 // import Register from "./pages/Register";
 // import Login from "./pages/Login";
 // import PageNotFound from "./pages/PageNotFound";
@@ -12,7 +14,7 @@ import Sidebar from './components/Sidebar';
 //     {
 //         path: "/",
 //         element: <Login></Login>,
-//     },
+//     },l
 //     {
 //         path: "/register",
 //         element: <Register></Register>,
@@ -28,19 +30,20 @@ import Sidebar from './components/Sidebar';
 // ]);
 
 const App = () => {
-    return (
-        <main className="app">
-            {/* <div className="relative w-full h-[100%] mx-auto bg-gradient-to-br from-tertiary to-secondary">
+  return (
+    <main className="app">
+      {/* <div className="relative w-full h-[100%] mx-auto bg-gradient-to-br from-tertiary to-secondary">
                 <RouterProvider router={router}></RouterProvider>
             </div>
             <div className="bg-primary bottom-[0px] fixed min-w-full">
                 <Footer />
             </div> */}
-            <div className="app__body">
-            <Sidebar />
-            </div>
-        </main>
-    );
+      <div className="app__body">
+        <Sidebar />
+        <Chat />
+      </div>
+    </main>
+  );
 };
 
 export default App;
