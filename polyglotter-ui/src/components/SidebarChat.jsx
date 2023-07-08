7c7cdac056bf1af71df66fda7f0118ae589ee7d3
Dean Byrne <deanbyrne95+github@gmail.com>
2023-07-08 17:32:02 +0100

    update SidebarChat to show formatted message date

--- polyglotter-ui/src/components/SidebarChat.jsx
+++ polyglotter-ui/src/components/SidebarChat.jsx
@@ -1,26 +1,29 @@
-import { useEffect, useState } from "react";
-import { Avatar } from "@mui/material";
+import {useEffect, useState} from "react";
+import {Avatar} from "@mui/material";
 
 import "./SidebarChat.css";
+import {formatDate} from "../App.jsx";
 
-const SidebarChat = ({ messages }) => {
-  const [seed, setSeed] = useState("");
-  useEffect(() => {
-    setSeed(Math.floor(Math.random() * 5000));
-  }, []);
+const SidebarChat = ({messages}) => {
+    const [seed, setSeed] = useState("");
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 5000));
+    }, []);
 
-  return (
-    <div className="sidebarChat">
-      <Avatar
-        src={`https://avatars.dicebear.com/api/human/
-b${seed}.svg`}
-      />
-      <div className="sidebarChat__info">
-        <h2>Dev Help</h2>
-        <p>{messages[messages.length -1]?.message}</p>
-      </div>
-    </div>
-  );
+    const lastMessage = messages[messages.length - 1];
+
+    return (
+        <div className="sidebarChat">
+            <Avatar
+                src={`https://avatars.dicebear.com/api/human/b${seed}.svg`}
+            />
+            <div className="sidebarChat__info">
+                <h2>Dev Help</h2>
+                <p>{lastMessage?.message}</p>
+                <span className="font-xxs">{formatDate(lastMessage?.timestamp, 'DD-MMM-YYYY @ HH:mm')}</span>
+            </div>
+        </div>
+    );
 };
 
 export default SidebarChat;
