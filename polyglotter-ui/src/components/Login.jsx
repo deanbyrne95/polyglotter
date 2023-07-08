2326c303694a0ee0a59e6919d71c615a21717083
Dean Byrne <deanbyrne95+github@gmail.com>
2023-07-08 17:33:41 +0100

    update login sign-in button; update app name

--- polyglotter-ui/src/components/Login.jsx
+++ polyglotter-ui/src/components/Login.jsx
@@ -2,18 +2,16 @@ import {Button} from "@mui/material";
 import "./Login.css";
 
 import {signInWithGoogle} from '../firebase.js'
-import {useStateValue} from "./StateProvider.jsx";
 
 const Login = () => {
-    const [{}, dispatch] = useStateValue();
     return (
         <div className="login">
             <div className="login__container">
                 <img src="/src/assets/polyglotter-icon10x.png" width={512} height={512} alt="logo"/>
                 <div className="login__text">
-                    <h1>Sign in to Messaging App</h1>
+                    <h1>Sign in to PolyGlotter</h1>
                 </div>
-                <Button onClick={() => signInWithGoogle(dispatch)}>Sign In with Google</Button>
+                <Button onClick={signInWithGoogle}>Sign In with Google</Button>
             </div>
         </div>
     );
